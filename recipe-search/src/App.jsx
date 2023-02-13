b6b9155ab4b8492cc8fd62e965a7e4ff5145e24e
Matthew <matthewkimcs@gmail.com>
2023-02-12 19:21:35 -0800

    first commit

---
+++ recipe-search/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [query, setQuery] = useState("chicken");
+  useEffect(() => {
+    fetchRecipeData();
+  }, []);
+  const fetchRecipeData = async () => {
+    const API_ID = "9c96dddd";
+    const API_KEY = "5ed5da612fdc934ee0dbfb39d541b822";
+    const response = await fetch(
+      `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${API_ID}&app_key=${API_KEY}`
+    );
+    const data = await response.json();
+    console.log(data);
+  };
+  return <div></div>;
+}
+
+export default App;
