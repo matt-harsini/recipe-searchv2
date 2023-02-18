777ed09639fe50139f444fc4097f87067dd5eff9
Matthew <matthewkimcs@gmail.com>
2023-02-17 17:31:25 -0800

    tweaked some css, increased font sizes and looking up fluid typography

--- recipe-search/src/components/Home/Home.jsx
+++ recipe-search/src/components/Home/Home.jsx
@@ -9,7 +9,13 @@ function Home() {
     <>
       <header className={styles.header}>
         <div className={styles.landing}>
-          <h1 className={styles.h1}>Recipe search</h1>
+          <div className={styles.info}>
+            <h1 className={styles.h1}>Recipe search</h1>
+            <p className={styles.p}>
+              Indulge in a culinary journey with our recipe website
+            </p>
+            <button className={styles.headerBtn}>Learn more</button>
+          </div>
         </div>
       </header>
     </>
