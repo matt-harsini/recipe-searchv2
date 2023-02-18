71b0ab2c81158df73165a783ec26ba800386b157
Matthew <matthewkimcs@gmail.com>
2023-02-18 13:02:40 -0800

    tweaked css styles, looking to implement framer motion

--- recipe-search/src/components/Home/Header.jsx
+++ recipe-search/src/components/Home/Header.jsx
@@ -1,16 +1,36 @@
 import React from "react";
 import styles from "./Header.module.css";
+import { motion } from "framer-motion";
+
 function Header() {
+  const cardVariants = {
+    offscreen: {
+      y: 300,
+    },
+    onscreen: {
+      y: 50,
+      rotate: -10,
+      transition: {
+        type: "spring",
+        bounce: 0.4,
+        duration: 0.8,
+      },
+    },
+  };
   return (
     <header className={styles.header}>
       <div className={styles.landing}>
-        <div className={styles.info}>
+        <motion.div
+          variants={cardVariants}
+          viewport={{ once: true }}
+          className={styles.info}
+        >
           <h1 className={styles.h1}>Recipe search</h1>
           <p className={styles.p}>
             Indulge in a culinary journey with our recipe website
           </p>
           <button className={styles.headerBtn}>Learn more</button>
-        </div>
+        </motion.div>
       </div>
     </header>
   );
