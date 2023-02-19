32f3d2712149250f0fc5dad67cd12b4f5a99c19f
Matthew <matthewkimcs@gmail.com>
2023-02-18 16:58:28 -0800

    adding another section in landing page

--- recipe-search/src/components/Home/Home.jsx
+++ recipe-search/src/components/Home/Home.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Header from "./Header";
-
+import Main from "./Main";
 function Home() {
   return (
     <>
       <Header />
+      <Main />
     </>
   );
 }
---
+++ recipe-search/src/components/Home/Main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Main.module.css";
+
+function Main() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.info}></div>
+    </main>
+  );
+}
+
+export default Main;
