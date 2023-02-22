048e7e304c5b2ed486ec373e1b8def1b6001effe
Matthew <matthewkimcs@gmail.com>
2023-02-22 09:52:15 -0800

    revamped fluid typescale design and worked on the main section of the landing page

--- recipe-search/src/components/Home/Main.jsx
+++ recipe-search/src/components/Home/Main.jsx
@@ -7,17 +7,29 @@ function Main({ innerRef }) {
       <div className={`${styles.container}`}>
         <span className={styles.subheading}>Plan meals</span>
         <h2 className={styles.heading}>
-          Search millions of recipes with nutritional info and step-by-step
-          instructions
+          Search a recipe database of over 2.3 million recipes
         </h2>
       </div>
       <section className={`${styles.container} ${styles.info}`}>
-        <div className={styles.text}>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas
-          ipsum qui sed ad in cum tempora illum ipsam voluptates sit omnis error
-          quasi voluptatibus, facilis deleniti aperiam beatae rerum mollitia!
+        <div>
+          <p className={styles.stepNum}>01</p>
+          <h3 className={styles.headingTertiary}>
+            500+ top web recipe sources
+          </h3>
+          <p className={styles.text}>
+            Our search algorithm returns the most relevant recipes from the most
+            popular and best recipes sources on the web. We order recipes by
+            their cookability and quality, so you can always count on getting
+            the best recipes!
+          </p>
         </div>
-        <div></div>
+        <div className={styles.stepImg}>
+          <img
+            className={styles.img}
+            src="../../../src/assets/159041840_l.jpg"
+            alt="Mushroom curry"
+          />
+        </div>
       </section>
     </main>
   );
