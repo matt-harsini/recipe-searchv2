c32747b1924712792fb464ee0ed81d6c5d48de19
Matthew <matthewkimcs@gmail.com>
2023-02-21 18:39:58 -0800

    started to work on the landing page by adding another section

--- recipe-search/src/components/Home/Main.jsx
+++ recipe-search/src/components/Home/Main.jsx
@@ -4,7 +4,21 @@ import styles from "./Main.module.css";
 function Main({ innerRef }) {
   return (
     <main className={styles.main} ref={innerRef}>
-      <div className={styles.info}></div>
+      <div className={`${styles.container}`}>
+        <span className={styles.subheading}>Plan meals</span>
+        <h2 className={styles.heading}>
+          Search millions of recipes with nutritional info and step-by-step
+          instructions
+        </h2>
+      </div>
+      <section className={`${styles.container} ${styles.info}`}>
+        <div className={styles.text}>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas
+          ipsum qui sed ad in cum tempora illum ipsam voluptates sit omnis error
+          quasi voluptatibus, facilis deleniti aperiam beatae rerum mollitia!
+        </div>
+        <div></div>
+      </section>
     </main>
   );
 }
