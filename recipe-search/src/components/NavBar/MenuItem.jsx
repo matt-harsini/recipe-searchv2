665a668d8dc64d83ad5fcc9fa71aa70fd61403c6
Matthew <matthewkimcs@gmail.com>
2023-03-05 11:53:44 -0800

    small tweaks

--- recipe-search/src/components/NavBar/MenuItem.jsx
+++ recipe-search/src/components/NavBar/MenuItem.jsx
@@ -23,6 +23,7 @@ const variants = {
 const colors = ["#e03131", "#e8590c", "#9C1AFF", "#7700FF", "#4400FF"];
 
 export function MenuItem({ i, url, text }) {
+  console.log(url);
   const toggleOpen = useContext(NavbarContext);
   const style = {
     borderBottom: `2.5px solid ${colors[i]}`,
@@ -41,10 +42,10 @@ export function MenuItem({ i, url, text }) {
         }}
         className={styles.btn}
       >
-        <NavLink to={url} className={styles.navLink}>
+        <NavLink to={url} className={`${styles.navLink}`}>
           {text}
+          <div className={styles.textPlaceholder} style={style} />
         </NavLink>
-        <div className={styles.textPlaceholder} style={style} />
       </button>
     </motion.li>
   );
