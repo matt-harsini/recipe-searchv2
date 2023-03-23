adaddfb4b3fadc1369988b90645eb7f6b5d16dd8
Matthew <matthewkimcs@gmail.com>
2023-03-22 20:11:13 -0700

    test

--- recipe-search/src/pages/Home/Home.jsx
+++ recipe-search/src/pages/Home/Home.jsx
@@ -1,14 +1,10 @@
 import React, { useEffect } from "react";
 import Header from "./Header";
 import Main from "./Main";
-import { useOutletContext } from "react-router-dom";
+
 function Home() {
-  const ref = useOutletContext();
   useEffect(() => {
     window.scrollTo(0, 0);
-    return () => {
-      ref[1].current.style.background = "#212529";
-    };
   }, []);
   return (
     <>
